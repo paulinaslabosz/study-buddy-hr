4efd40d3c5112fc43f8569c993dbd49570372a7b
Paulina Słabosz <sejmipool@gmail.com>
2022-01-12 22:03:46 +0100

    configuration CRA before push to github

--- src/views/App.js
+++ src/views/App.js
@@ -1,22 +1,17 @@
 import logo from 'assets/logo.svg';
 import './App.css';
 
-
 console.log('hello');
-
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit hehe <code>src/App.js</code> and save to reload.
         </p>
         <a
-
-        
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
